a85a46e9d600c6f23d9d55c54a6a2d605fc74131
韩宇潇_16340069 <1078518902@qq.com>
2018-12-02 23:24:52 +0800

    small change of Server send

--- src/lftp/Client.java
+++ src/lftp/Client.java
@@ -4,8 +4,6 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.security.cert.TrustAnchor;
 import java.util.Arrays;
 import java.util.Scanner;
 
--- src/useless/UdpClient.java
+++ src/useless/UdpClient.java
@@ -1,4 +1,4 @@
-package lftp;
+package useless;
 
 import java.io.IOException;
 import java.io.InterruptedIOException;
